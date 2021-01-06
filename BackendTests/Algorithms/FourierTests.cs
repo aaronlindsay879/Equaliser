906cdfeb48bd005fea614f3563e61eb6c36c254c
Aaron <aarlin5409@hessleacademy.com>
2021-01-06 18:00:17 +0000

    added initial (non working) tests

---
+++ BackendTests/Algorithms/FourierTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Backend.Algorithms;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Backend.Data;
+using System.Linq;
+
+namespace Backend.Algorithms.Tests
+{
+    [TestClass()]
+    public class FourierTests
+    {
+        [TestMethod()]
+        public void DFTTest()
+        {
+            /*            Song song = new Song()
+                        {
+                            SamplingRate = 10,
+                            Sound = Enumerable.Range(0, 100).Select(x => Math.Sin(x * Math.PI / 25)).ToArray()
+                        };
+
+                        foreach (var (x, i) in song.DFT().Audio.Select((x, i) => (x, i)))
+                        {
+                            Console.WriteLine($"Freq: {i}, Amp: {x}");
+                        }*/
+
+
+            byte[] data = FileHandling.Wav.ReadWav("../../../Files/440.wav");
+            FileHandling.Wav.ParseWav(data);
+        }
+    }
+}
---
+++ BackendTests/GenerateWave.cs
@@ -0,0 +1,15 @@
+﻿using Backend.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BackendTests
+{
+    public static class GenerateWave
+    {
+        public static Song GenWave(int samplingRate, int frequency, int amplitude, int waves = 1)
+        {
+            return new Song();
+        }
+    }
+}

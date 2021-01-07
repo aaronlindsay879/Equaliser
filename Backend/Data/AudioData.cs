33bf72005dd7dcd499454e79d7e82d438035ae8e
Aaron <aarlin5409@hessleacademy.com>
2021-01-07 20:42:57 +0000

    implemented equality operators for song and audio data

--- Backend/Data/AudioData.cs
+++ Backend/Data/AudioData.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Text;
 
 namespace Backend.Data
 {
-    public class AudioData
+    public class AudioData : IEquatable<AudioData>
     {
         public short NumChannels;
         public int SampleRate;
@@ -14,5 +15,13 @@ namespace Backend.Data
         public short BytesPerSample => (short)(BitsPerSample / 8);
         public int Subchunk2Size => NumSamples * NumChannels * BytesPerSample;
         public int ByteRate => SampleRate * NumChannels * BytesPerSample;
+
+        public bool Equals(AudioData other)
+        {
+            return NumChannels == other.NumChannels
+                && SampleRate == other.SampleRate
+                && BitsPerSample == other.BitsPerSample
+                && NumSamples == other.NumSamples;
+        }
     }
 }
--- Backend/Data/Song.cs
+++ Backend/Data/Song.cs
@@ -1,15 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 using System.Text;
 
 namespace Backend.Data
 {
-    public class Song
+    public class Song : IEquatable<Song>
     {
         public double[] Sound;
         public AudioData Data;
 
         //kept in temp
         public int SamplingRate;
+
+        public bool Equals(Song other)
+        {
+            return Enumerable.SequenceEqual(Sound, other.Sound)
+                && Data.Equals(other.Data);
+        }
     }
 }

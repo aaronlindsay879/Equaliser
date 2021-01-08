b99bf6439498b315676cd7090a5482779be58969
Aaron <aarlin5409@hessleacademy.com>
2021-01-08 15:48:14 +0000

    added comments for bitinfo

--- Backend/Data/BitInfo.cs
+++ Backend/Data/BitInfo.cs
@@ -1,10 +1,17 @@
 ﻿namespace Backend.Data
 {
+    /// <summary>
+    /// Struct containing information on the location and size of data within a byte array
+    /// </summary>
     public struct BitInfo
     {
         public int Location;
         public int Bits;
 
+        /// <summary>
+        /// Constructs a BitInfo from a tuple assignment
+        /// </summary>
+        /// <param name="tuple">Tuple wherre item1 = location, item2 = bit length</param>
         public static implicit operator BitInfo((int, int) tuple) => new BitInfo
         {
             Location = tuple.Item1,

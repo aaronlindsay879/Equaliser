d37522562721dac57cf43511689a8bfe4131e6f0
Aaron <aarlin5409@hessleacademy.com>
2021-02-02 09:19:28 +0000

    cleaned up testing code

--- BackendTests/FileHandling/WavTests.cs
+++ BackendTests/FileHandling/WavTests.cs
@@ -10,7 +10,7 @@ namespace Backend.FileHandling.Tests
         [TestMethod()]
         public void ReadTest()
         {
-            Song song = Wav.Read(@"../../../Files/1.wav");
+            Song song = Wav.Read(@"Files/1.wav");
 
             Assert.AreEqual(1, song.Data.NumChannels);
             Assert.AreEqual(100, song.Data.SampleRate);
@@ -19,9 +19,9 @@ namespace Backend.FileHandling.Tests
         }
 
         [TestMethod()]
-        [DataRow(@"../../../Files/1.wav")]
-        [DataRow(@"../../../Files/440.wav")]
-        [DataRow(@"../../../Files/1 2 3.wav")]
+        [DataRow(@"Files/1.wav")]
+        [DataRow(@"Files/440.wav")]
+        [DataRow(@"Files/1 2 3.wav")]
         public void SaveIntegrationTest(string filePath)
         {
             Song origSong = Wav.Read(filePath);
